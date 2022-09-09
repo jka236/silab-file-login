9759630f9f4a632042f773c86c6edf2e9ebbc2f6
Jonghyeok K <jka236@sfu.ca>
2022-09-08 23:17:07 -0700

    init shell creating venv

--- init-shell.sh
+++ init-shell.sh
@@ -3,4 +3,12 @@
 export FLASK_APP=project
 export FLASK_DEBUG=1
 
-flask run --host=0.0.0.0 --port=4000
+VENV=$(dirname "${BASH_SOURCE[0]}")/venv
+
+if [ ! -d "$VENV" ]; then
+    echo "Create venv"
+    python -m venv ./venv
+fi
+
+source ./venv/bin/activate
+pip install -r requirements.txt && flask run --host=0.0.0.0 --port=4000
